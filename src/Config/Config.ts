39fd7f81c579dd66875c847dade66db473036971
Marko Buxar <markobuxar@Markos-MacBook-Pro.local>
2022-05-13 14:41:16 +0200

    added prod config

--- src/Config/Config.ts
+++ src/Config/Config.ts
@@ -1,4 +1,5 @@
 import config from './config.json';
+import prod from './prod.json';
 import path from 'path';
 import fs from 'fs-extra';
 import _ from 'lodash';
@@ -6,34 +7,42 @@ import { Logger } from '../Logger/Logger';
 
 export class Config {
   private static _instance: Config;
+  private confPath;
+  private conf;
+
+  constructor() {
+    this.confPath = process.env.PRODUCTION ? './prod.json' : 'config.json';
+    this.conf = process.env.PRODUCTION ? prod : config;
+    console.log(this.confPath);
+  }
 
   public static get Instance() {
     return this._instance || (this._instance = new this());
   }
 
   public Get(p: string): any {
-    return _.get(config, p);
+    return _.get(this.conf, p);
   }
 
   public Set(p: string, value: any): object {
-    _.set(config, p, value);
+    _.set(this.conf, p, value);
     this.Save();
-    return config;
+    return this.conf;
   }
 
   public Has(p: string): boolean {
-    return _.has(config, p);
+    return _.has(this.conf, p);
   }
 
   public Remove(p: string) {
-    _.unset(config, p);
+    _.unset(this.conf, p);
     this.Save();
   }
 
   private Save() {
     try {
       fs.writeFileSync(
-        path.join(__dirname, 'config.json'),
+        path.join(__dirname, this.confPath),
         JSON.stringify(config, null, 2),
       );
     } catch (exp) {
